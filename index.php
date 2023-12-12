4dfda04b3634ab1fc4cff621a8f8131049bd8c05
Jakhongir Olimov <83121159+olimovdev@users.noreply.github.com>
2023-12-12 11:16:34 +0500

    chatGPD da boshqa kod

--- index.php
+++ index.php
@@ -1,27 +1,58 @@
 <?php
-require 'vendor/autoload.php';
+// Telegram Bot API tokeni
+$botToken = "6511859075:AAG_PJoc3rB7KcSywL_vFraJn6V35BDs1P4";
+// chat1 guruh IDsi
+$chat1GroupId = -1002076251830;
+// chat2 guruh IDsi
+$chat2GroupId = -1002044782224;
 
-use Telegram\Bot\Api;
+// Telegram Bot API so'rov yuborish uchun asosiy manzil
+$apiUrl = "https://api.telegram.org/bot6511859075:AAG_PJoc3rB7KcSywL_vFraJn6V35BDs1P4";
 
-// Bot tokenni to'ldiring
-$token = '6511859075:AAG_PJoc3rB7KcSywL_vFraJn6V35BDs1P4';
+// Chat1 guruhidagi administratorlarni olish
+$admins = getChatAdministrators($chat1GroupId);
 
-// Chat ID ni to'ldiring (chat1 guruhining ID si)
-$chat1Id = '-1002076251830';
-
-// Chat2 guruhining ID sini to'ldiring
-$chat2Id = '-1002044782224';
-
-// Telegram Bot API ga ulangan obyekt yaratish
-$telegram = new Api($token);
-
-// Chat1 guruhidagi xabarlarini olish
-$response = $telegram->getChatAdministrators(['chat_id' => $chat1Id]);
-
-// Chat2 guruhiga xabarlarini yuborish
-foreach ($response as $admin) {
-    $message = "Admin: @" . $admin->getUser()->getUsername() . " - " . $admin->getStatus();
-    $telegram->sendMessage(['chat_id' => $chat2Id, 'text' => $message]);
+// Chat1 guruhidagi administratorlarning IDlarini olamiz
+$adminIds = array();
+foreach ($admins as $admin) {
+    $adminIds[] = $admin['user']['id'];
 }
 
-// echo "Xabarlar yuborildi!";
+// Chat1 guruhidagi xabarlarni olish va chat2 guruhiga yuborish
+$updates = getGroupMessages($chat1GroupId);
+
+foreach ($updates as $update) {
+    $message = $update['message'];
+    $userId = $message['from']['id'];
+
+    // Faqat guruh administratorlarining xabarlari chat2 guruhiga yuboriladi
+    if (in_array($userId, $adminIds)) {
+        forwardMessage($chat2GroupId, $chat1GroupId, $message['message_id']);
+    }
+}
+
+// Chatdagi administratorlarni olish
+function getChatAdministrators($chatId) {
+    global $apiUrl;
+    $url = "$apiUrl/getChatAdministrators?chat_id=$chatId";
+    $response = file_get_contents($url);
+    $result = json_decode($response, true);
+    return $result['result'];
+}
+
+// Guruhdagi xabarlarni olish
+function getGroupMessages($chatId) {
+    global $apiUrl;
+    $url = "$apiUrl/getUpdates?offset=-1&limit=1&chat_id=$chatId";
+    $response = file_get_contents($url);
+    $result = json_decode($response, true);
+    return $result['result'];
+}
+
+// Xabarni boshqa guruhga yuborish
+function forwardMessage($toChatId, $fromChatId, $messageId) {
+    global $apiUrl;
+    $url = "$apiUrl/forwardMessage?chat_id=$toChatId&from_chat_id=$fromChatId&message_id=$messageId";
+    file_get_contents($url);
+}
+?>
