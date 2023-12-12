8ba2fb4f99dac615cb5ccf147b8c3701c3dba569
Jakhongir Olimov <83121159+olimovdev@users.noreply.github.com>
2023-12-12 10:22:43 +0500

    chatGPT ishlab berdi

--- index.php
+++ index.php
@@ -1,49 +1,27 @@
 <?php
-$params = [
-    'token' => '6511859075:AAG_PJoc3rB7KcSywL_vFraJn6V35BDs1P4', #token
-    'bot_url' => 'https://api.telegram.org/bot',
-];
+require 'vendor/autoload.php';
 
+use Telegram\Bot\Api;
 
-// ========== webhook =========
- $request = json_decode(file_get_contents('php://input'), TRUE);
-// ========== webhook =========
+// Bot tokenni to'ldiring
+$token = '6511859075:AAG_PJoc3rB7KcSywL_vFraJn6V35BDs1P4';
 
+// Chat ID ni to'ldiring (chat1 guruhining ID si)
+$chat1Id = '@taksitoshfar';
 
-$message = $request['message'];
+// Chat2 guruhining ID sini to'ldiring
+$chat2Id = '@toshfarzakaz';
 
+// Telegram Bot API ga ulangan obyekt yaratish
+$telegram = new Api($token);
 
+// Chat1 guruhidagi xabarlarini olish
+$response = $telegram->getChatAdministrators(['chat_id' => $chat1Id]);
 
-switch($message['text']){
-    case '/start':
-        $method = 'sendMessage';
-        $send_data = [
-           'text' => '<pre>'.$message.'</pre>',
-        ];
-        break;
-
+// Chat2 guruhiga xabarlarini yuborish
+foreach ($response as $admin) {
+    $message = "Admin: @" . $admin->getUser()->getUsername() . " - " . $admin->getStatus();
+    $telegram->sendMessage(['chat_id' => $chat2Id, 'text' => $message]);
 }
 
-$send_data['chat_id'] = $message['chat']['id'];
-
-sendTelegram($params, $method, $send_data);
-
-
-
-function sendTelegram($params, $method, $data=[], $headers=[]){
-    $curl = curl_init();
-
-    curl_setopt_array($curl,[
-        CURLOPT_POST => 1,
-        CURLOPT_HEADER => 0,
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $params['bot_url'].$params['token'].'/'.$method,
-        CURLOPT_POSTFIELDS => json_encode($data),
-        CURLOPT_HTTPHEADER => array_merge(["Content-Type: application/json"], $headers)
-
-    ]);
-
-    $result = curl_exec($curl);
-    curl_close($curl);
-    return json_decode($result,1) ?: $result;
-}
+echo "Xabarlar yuborildi!";
